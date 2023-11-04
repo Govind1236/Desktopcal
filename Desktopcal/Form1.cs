798ea6c14b77fe067c5a7f4db9d4ebbfd77c3a79
TCS <TCS@LAPTOP-R08T1R13>
2023-11-04 12:43:06 +0545

    Add project files.

---
+++ Desktopcal/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desktopcal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string cal;
+        int n1 , n2 , result ;
+        string opt;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+                           
+            textBox1.Text = textBox1.Text + "1";
+        }
+       
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "7";
+
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "9";
+        }
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            opt = "+";
+            n1 = int.Parse(textBox1.Text);
+            
+            
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "0";
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "";
+        }
+
+    }
+}
